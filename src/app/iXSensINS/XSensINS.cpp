0b21acda258cb21b06d728a5503a3a97bc20a6cc
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2015-09-21 11:44:51 +0200

    [src] minor corrections Xsens

--- src/app/iXSensINS/XSensINS.cpp
+++ src/app/iXSensINS/XSensINS.cpp
@@ -101,9 +101,9 @@ bool XSensINS::Iterate() {
     //Gyro
     if(packet.containsCalibratedGyroscopeData()){
       m_gyro = packet.calibratedGyroscopeData();
-      Notify("IMU_GYR_X", m_gyro[0]);
-      Notify("IMU_GYR_Y", m_gyro[1]);
-      Notify("IMU_GYR_Z", m_gyro[2]);
+      Notify("IMU_GYR_X", m_gyro[0]*180.0/M_PI);
+      Notify("IMU_GYR_Y", m_gyro[1]*180.0/M_PI);
+      Notify("IMU_GYR_Z", m_gyro[2]*180.0/M_PI);
     }
 
     //Magneto
@@ -164,10 +164,11 @@ bool XSensINS::OnStartUp() {
   }
   
   registerVariables();
-  
+
   //------ OPEN INS ---------------//
   XsPortInfo mtPort("/dev/xsens", XsBaud::numericToRate(115200));
   if (!m_device.openPort(mtPort)) {
+    cout << "CANNOT OPEN THE PORT" << '\n';
     reportRunWarning("Could not open the COM port" + m_uart_port);
   }
 
