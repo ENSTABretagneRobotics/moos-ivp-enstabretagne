4f3412a09ba579ae04fd1e5618d6ba82028140c6
SimonRohou <simon.rohou@gmail.com>
2015-09-21 09:10:10 +0200

    [src, param] pPID coeff + GYR in degree/s

--- src/app/iXSensINS/XSensINS.cpp
+++ src/app/iXSensINS/XSensINS.cpp
@@ -101,9 +101,9 @@ bool XSensINS::Iterate() {
     //Gyro
     if(packet.containsCalibratedGyroscopeData()){
       m_gyro = packet.calibratedGyroscopeData();
-      Notify("IMU_GYR_X", m_gyro[0]);
-      Notify("IMU_GYR_Y", m_gyro[1]);
-      Notify("IMU_GYR_Z", m_gyro[2]);
+      Notify("IMU_GYR_X", m_gyro[0]*180.0/M_PI);
+      Notify("IMU_GYR_Y", m_gyro[1]*180.0/M_PI);
+      Notify("IMU_GYR_Z", m_gyro[2]*180.0/M_PI);
     }
 
     //Magneto
