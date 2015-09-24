60c9716138cc7f037f0691fcd847d552ba6370bc
SimonRohou <simon.rohou@gmail.com>
2015-09-24 10:11:58 +0200

    [rush] added pOpiDetection moos config files + rad2Degree + degree2Rad + DESIRED_HEADING_RAD

---
+++ src/app/pDegree2Rad/Degree2Rad.h
@@ -0,0 +1,39 @@
+/************************************************************/
+/*    FILE: Degree2Rad.h
+/*    ORGN: Toutatis AUVs - ENSTA Bretagne
+/*    AUTH: Simon Rohou
+/*    DATE: 2015
+/************************************************************/
+
+#ifndef Degree2Rad_HEADER
+#define Degree2Rad_HEADER
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+
+class Degree2Rad : public AppCastingMOOSApp
+{
+  public:
+    Degree2Rad();
+    ~Degree2Rad() {};
+
+  protected: // Standard MOOSApp functions to overload  
+    bool OnNewMail(MOOSMSG_LIST &NewMail);
+    bool Iterate();
+    bool OnConnectToServer();
+    bool OnStartUp();
+
+  protected: // Standard AppCastingMOOSApp functions to overload 
+    bool buildReport();
+    void registerVariables();
+
+  protected: // Degree2Rad functions
+
+
+  private: // Configuration variables
+
+
+  private: // State variables
+
+};
+
+#endif 
