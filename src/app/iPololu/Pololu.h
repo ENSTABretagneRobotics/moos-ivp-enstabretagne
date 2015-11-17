3cbfa6d83ace0f19dfd6d69787afdc2ae86f5ff3
SimonRohou <simon.rohou@gmail.com>
2015-11-17 10:01:34 +0100

    [src] added iPololu

---
+++ src/app/iPololu/Pololu.h
@@ -0,0 +1,38 @@
+/************************************************************/
+/*    FILE: Pololu.h
+/*    ORGN: Toutatis AUVs - ENSTA Bretagne
+/*    AUTH: Simon Rohou
+/*    DATE: 2015
+/************************************************************/
+
+class Pololu;
+
+#ifndef Pololu_HEADER
+#define Pololu_HEADER
+
+#include <QObject>
+
+class Pololu : public QObject
+{
+  Q_OBJECT
+
+  public:
+    Pololu(std::string device_name);
+    ~Pololu();
+    
+    bool isReady();
+    bool isReady(std::string &error_message);
+
+  public slots:
+    bool setTarget(unsigned char channel, unsigned short target);
+
+  protected:
+    void setErrorMessage(std::string message);
+    
+  protected:
+    int m_pololu;
+    bool m_error;
+    std::string m_error_message;
+};
+
+#endif 
---
+++ src/app/iPololu/PololuApp.h
@@ -0,0 +1,41 @@
+/************************************************************/
+/*    FILE: PololuApp.h
+/*    ORGN: Toutatis AUVs - ENSTA Bretagne
+/*    AUTH: Simon Rohou
+/*    DATE: 2015
+/************************************************************/
+
+#ifndef PololuApp_HEADER
+#define PololuApp_HEADER
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+#include "Pololu.h"
+#include "PololuPinOut.h"
+
+class PololuApp : public AppCastingMOOSApp
+{
+  public:
+    PololuApp();
+    ~PololuApp();
+
+  protected: // Standard MOOSApp functions to overload  
+    bool OnNewMail(MOOSMSG_LIST &NewMail);
+    bool Iterate();
+    bool OnConnectToServer();
+    bool OnStartUp();
+
+  protected: // Standard AppCastingMOOSApp functions to overload 
+    bool buildReport();
+    void registerVariables();
+
+  protected: // PololuApp functions
+
+  private: // Configuration variables
+    std::string m_device_name;
+
+  private: // State variables
+    Pololu *m_pololu;
+    std::map<std::string,PololuPinOut*> m_map_pinouts;
+};
+
+#endif 
