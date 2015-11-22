a95d45dea21a86df10e16939f3d0442258fc930a
SimonRohou <simon.rohou@gmail.com>
2015-11-22 14:27:15 +0100

    [all] added first install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------
+#  Part 1: Building moos-ivp
+#-------------------------------------------------------------------
+
+cd
+svn co https://oceanai.mit.edu/svn/moos-ivp-aro/releases/moos-ivp-14.7.1 moos-ivp
+cd moos-ivp
+sudo apt-get update -q
+sudo apt-get install -y --force-yes g++ subversion xterm cmake libfltk1.3-dev freeglut3-dev libpng12-dev libjpeg-dev libxft-dev libxinerama-dev libtiff5-dev
+./build-moos.sh
+./build-ivp.sh
+
+echo 'export PATH=$PATH:~/moos-ivp/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:~/moos-ivp/lib' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/moos-ivp/lib' >> ~/.bashrc
+
+source ~/.bashrc
+
+#-------------------------------------------------------------------
+#  Part 2: Building moos-ivp-toutatis
+#-------------------------------------------------------------------
+
+cd ../moos-ivp-toutatis
+sudo apt-get install -y --force-yes libtinyxml2-dev libusb-1.0-0-dev mono-runtime libmono-winforms2.0-cil libv4l-dev libopencv-dev libboost1.55-dev libboost-system1.55-dev
+./build.sh
+
+echo 'export PATH=$PATH:~/moos-ivp-toutatis/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:~/moos-ivp-toutatis/lib' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/moos-ivp-toutatis/lib' >> ~/.bashrc
+echo 'export IVP_BEHAVIOR_DIRS=$IVP_BEHAVIOR_DIRS:~/moos-ivp-toutatis/lib' >> ~/.bashrc
+echo 'export MOOS_IVP_TOUTATIS_PATH="/home/'$USER'/moos-ivp-toutatis"' >> ~/.bashrc
+
+source ~/.bashrc
