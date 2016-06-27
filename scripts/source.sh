ce33eaef94c09ddc4f77d05fea75df67721ef89b
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2016-06-27 13:59:19 +0200

    [Scripts] Put all export commands and alias into a source.sh file.

--- scripts/source.sh
+++ scripts/source.sh
@@ -1,16 +1,15 @@
-
-export PATH=$PATH:~/moos-ivp-enstabretagne/bin
-export PATH=$PATH:~/moos-ivp-enstabretagne/lib
-export PATH=$PATH:~/moos-ivp-enstabretagne/scripts
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/moos-ivp-enstabretagne/lib
-export IVP_BEHAVIOR_DIRS=$IVP_BEHAVIOR_DIRS:~/moos-ivp-enstabretagne/lib
-export MOOS_IVP_ENSTABRETAGNE_PATH="/home/ciscrea/moos-ivp-enstabretagne"
+export MOOS_IVP_ENSTABRETAGNE_PATH=$(dirname `dirname $BASH_SOURCE`)
+export PATH=$PATH:$MOOS_IVP_ENSTABRETAGNE_PATH/bin
+export PATH=$PATH:$MOOS_IVP_ENSTABRETAGNE_PATH/lib
+export PATH=$PATH:$MOOS_IVP_ENSTABRETAGNE_PATH/scripts
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MOOS_IVP_ENSTABRETAGNE_PATH/lib
+export IVP_BEHAVIOR_DIRS=$IVP_BEHAVIOR_DIRS:$MOOS_IVP_ENSTABRETAGNE_PATH/lib
 
 
 alias createdevelopbranch='git checkout master && git branch -D develop | git checkout -b develop && git merge uJoystick missions scripts'
 
 alias cdEnstaMissions='cd $MOOS_IVP_ENSTABRETAGNE_PATH/missions'
-alias cdEnstaMissions='cd $MOOS_IVP_ENSTABRETAGNE_PATH/missions/saucisse/rov'
+alias cdSaucisseMissions='cd $MOOS_IVP_ENSTABRETAGNE_PATH/missions/saucisse/rov'
 
-alias connectSaucisseE='ssh ciscrea@192.168.0.100:$MOOS_IVP_ENSTABRETAGNE_PATH/missions/saucisse/rov -X'
-alias connectSaucisseW='ssh ciscrea@192.168.0.101:$MOOS_IVP_ENSTABRETAGNE_PATH/missions/saucisse/rov -X'
+alias connectSaucisseW='ssh ciscrea@192.168.0.101 -x'
+alias connectSaucisseE='ssh ciscrea@192.168.0.100 -x'
